1e93a9979b1e909b3a7f84975e2bdb35acd94e2e
Nikita Vitkovskiy <nicknknecht@gmail.com>
2026-07-07 23:58:12 +0000

    feat(simple): Requirement-station mechanism is fully mapped out: type erased CRTP on the Station side, double dispatching on the Requirement side, all enforced and declared by C++20 concepts.

---
+++ 3/model/common/requirement/requirement.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+namespace sub{
+//template<typename>
+class Requirement;
+}
+
+#include <stdint.h>
+#include <string>
+
+namespace sub {
+class Station;
+}
+
+namespace sub {
+
+template<typename R>
+concept is_Req = requires(const R& t, const Requirement& b, Station& s, const Station& sc) {
+	{t <=> b} -> std::convertible_to<bool>;
+	{t <=> t} -> std::convertible_to<bool>;
+	{b <=> t} -> std::convertible_to<bool>;
+	{t.dispatchTryFix(s)} -> std::convertible_to<bool>;
+	{t.dispatchVerify(sc)} -> std::convertible_to<bool>;
+} && std::is_base_of_v<Requirement, R>;
+
+enum class RequirementPriority : uint32_t {
+	ReqLinesExist,
+	ReqStationExistsNotOnLine
+};
+
+#include "../../station/station.hpp"
+
+
+/**/
+class Requirement{
+private:
+	std::string source;
+	RequirementPriority priority;
+public:
+	bool operator<(const Requirement& other) const noexcept; //!< Priority -> name comparion
+	bool operator==(const Requirement& other) const noexcept; //!< Priority -> name comparison
+	bool operator<=>(const Requirement& other) const noexcept; //!< Priority -> name comparison
+
+	Requirement() = delete;
+	Requirement(const Requirement&) = default;
+	Requirement(Requirement&&) = default;
+	Requirement& operator=(const Requirement&) = default;
+	Requirement& operator=(Requirement&&) = default;
+	~Requirement() = default;
+
+	virtual bool dispatchTryFix(Station&s) = 0; // {return s.tryFix(*this);} // TODO: override looks the same for all req
+	virtual bool dispatchVerify(const Station&s) = 0; // {return s.tryFix(*this);} // TODO: override this with auto in templates for all req classes
+
+	static_assert(is_Req<Requirement>);
+};
+
+class ReqLinesExist : public Requirement {};
+static_assert(is_Req<ReqLinesExist>);
+
+/*! @brief Class that manages requirements posed by one station onto another. These can later be checked by the Subway class.
+ * 
+ * The guarantee is given that cheking requirements of lower priority does not break already checked requirements of higher priorities.
+ * Copies are allowed, it is reqsonsibility of the subway class to check that a pointer to the same source doesn't get written into the delete list twice.
+ * Source is refered to by string as a more robust and safer identity that already has all the infrustructure for working with it.
+ * Requirement sorting has to account for both priority and the source name. This is because, when processed in parallel, each thread has to get all the requirements on the same priority for one station.
+ * Co-dependent requirements are prohiboted because they would violate the priority guarantee and therefore require special requirement classes. But this should not occur in a typical metro anyway.
+ *
+ * @see Double dispatch pattern for info on repairing stations
+ */
+class TypeErasedRequirement {
+protected:
+	Requirement pimpl; //!< @see Type erasure design pattern
+public:
+	/*std::string source; //!< We must be able to perform a search by name even on a deleted station (for example, by a direct rm command)
+protected:
+	Requirement(const std::string_view src);
+	virtual constexpr RequirementPriority kind() const noexcept = 0; // !< This is nessesary so that comparisons work, and so that the source can tryFix the unsatisfied requirement, which means it has to know the requirement kind. Polymorphism here allown easy comparisons 
+public:
+	bool operator<(const Requirement& other) const noexcept; //!< Priority -> name comparion
+	bool operator==(const Requirement& other) const noexcept; //!< Priority -> name comparison
+	bool operator<=>(const Requirement& other) const noexcept; //!< Priority -> name comparison
+
+	Requirement() = delete;
+	Requirement(const Requirement&) = default;
+	Requirement(Requirement&&) = default;
+	Requirement& operator=(const Requirement&) = default;
+	Requirement& operator=(Requirement&&) = default;
+	~Requirement() = default;
+	*/
+};
+
+}
--- 3/model/station/station.hpp
+++ 3/model/station/station.hpp
@@ -1,28 +1,138 @@
 #pragma once
 
+#include <memory>
+
 namespace sub {
 class Station;
 }
 
-#include "../subway/deps/requirement/requirement.hpp"
-
-#include <string>
-#include <vector>
+#include "../common/requirement/requirement.hpp"
 
 namespace sub {
+class Station;
 
-/*! @brief Class that represents a station entry.
- *
- * Owning lines were replaced with std::string line entry in each Station, as an unnessasary complication that would requier  a lot of complexity in verification.
- */
-class Station {
-private:
-	std::string name;
-	std::string line;
+namespace sub1{
+template<is_Req Req>
+class ReqStationStrategy {
 public:
-	virtual std::vector<sub::Requirement*> req() = 0;
-	virtual bool verify(const std::vector<sub::Requirement>& check) = 0;
-	virtual bool tryFix(const sub::Requirement& unsatisfied) = 0;
+	bool verify(const Station&, const Req&) const noexcept;
+	bool tryFix(Station&);
 };
 
+/*! This is a type erased station class. 
+ *
+ * Type erasure is nessesary because instances of the would be stored as the base class. Apart from that, each station class
+ * needs to have strategies of working with different Requirements according to the strategy design pattern, virtual templated 
+ * functions are not allowed in C++.
+ *
+ * @see Type erasure design pattern
+ * @see Bridge design pattern
+ * @see Prototype design pattern
+ * @see Strategy design pattern
+ * */
+class Station {
+private:
+	class StationConcept {};
+
+	template<class StationT>
+	class StationModel : public StationConcept{
+		friend bool verify(const StationT&, const is_Req auto&); //!< Defining specializations these is resposibility of requirements.
+		friend bool tryFix(StationT&, const is_Req auto&); //!< Defining specializations of these is resposibility of requirements.
+	
+		bool do_verify(const is_Req auto&);
+		bool tryFix(const is_Req auto&);
+
+	};
+
+	bool do_verify(const is_Req auto&);
+	bool do_tryFix(const is_Req auto&);
+
+	std::unique_ptr<StationConcept> pimpl;
+};
+
+template<>
+bool Station::do_tryFix(const Requirement&) {
+	return false;
 }
+
+}
+// -----------------------------------------------------------------------------------------------------------------------------------------------
+// _______________________________________________________________________________________________________________________________________________
+template <typename Station>
+class StationCRTP {
+	std::unique_ptr<Station> it;
+public:
+	bool tryFix(const is_Req auto&);
+	bool verify(const is_Req auto&) const;
+};
+
+class DirectStation : public StationCRTP<DirectStation> {
+
+};
+
+/*! Type erased station 
+ *
+ * @example Works!! This below is responsibility of the Requirements*/
+template<>
+template<>
+bool StationCRTP<DirectStation>::tryFix<Requirement>(const Requirement&) {
+	return false;
+}
+template<>
+template<>
+bool StationCRTP<DirectStation>::verify<Requirement>(const Requirement&) const {
+	return false;
+}
+// ^ New requirement types are supposed to do this ^
+
+class Station {
+	class StationConcept {
+		virtual bool tryFix(const sub::Requirement&);	
+		virtual bool verify(const sub::Requirement&) const;
+	};
+
+	// TODO: Require all station to have tryFix and verify through concepts
+	// TODO CRTP for ~(~i~n place o~f~)~ double dispatch
+	// TODO Type erasure (with only constructors) for storing the objects
+
+
+	// StationT is any CRTP. This is the main buf of type erasure is that erased typed don't have to have the same base class
+	template<class StationT>
+	class StationModel : StationConcept {
+		bool tryFix(const sub::Requirement& it) override {
+			tryFix(obj, it);
+		};
+		bool verify(const sub::Requirement& it) const override {
+			verify(obj, it);
+		}
+
+		StationT obj;
+	};
+
+	std::unique_ptr<StationConcept> pimpl;
+};
+
+//!< _ @example _
+bool tryFix(DirectStation& s, is_Req auto); //!< @see External polymorphism
+bool verify(const DirectStation& s, is_Req auto); //!< @see External polymorphism
+// ^ New station types are supposed to do this ^
+// TODO Declare these for all stations at once with concepts
+
+//!< @example
+bool tryFix(DirectStation& s, Requirement r) {
+	s.tryFix(r);
+}
+bool verify(const DirectStation& s, Requirement r) {
+	s.verify(r);
+}
+// ^ New requirement types are supposed to do this ^
+// Actually, this could probably be done either by every new station with templates,
+// or for all stations at once with concepts + templates
+// TODO: But the problem is, if requirements are stored polymorphically as base class, calling 
+// Requirement req {concreteReqRvalue}; Station s{...}; s.verify(req); Will call verify version for the Requirement base class
+// Need either another type erasure on Requirement too, or, more interestingly, double dispatch.
+// TODO Double dispatch the sucker
+// Or... Do I need double dispatch thx to type erasure? Single dispatch is prolly enough
+
+}
+
